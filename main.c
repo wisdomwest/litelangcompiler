afb5966f18a8ae018c79f1f11cfb1b42a0d273ed
wisdomwest <harmonykoome@gmail.com>
2024-01-31 15:53:46 +0300

    okay i structure for lexer established now to work on it

--- main.c
+++ main.c
@@ -1,55 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-#include "token.h"
+#include "lexer.h"
 
 /**
-  * lexer - lexer function
-  * @file: pointer to llc file
+  * main - entry point of compiler
   * Return: void
-*/
-void lexer(FILE *file)
-{
-	char current;
+  */
 
-	current = fgetc(file);
-
-	while (current != EOF)
-	{
-		if (current == ';')
-		{
-			printf("FOUND SEMICOLON\n");
-		}
-		else if (current == '(')
-		{
-			printf("FOUND OPEN PAREN\n");
-		}
-		else if (current == ')')
-		{
-			printf("FOUND CLOSE PAREN\n");
-		}
-		else if (isdigit(current))
-		{
-			printf("FOUND DIGIT: %d\n", current - '0');
-		}
-		else if (isalpha(current))
-		{
-			printf("FOUND CHARACTER: %c\n", current);
-		}
-		current = fgetc(file);
-	}
-}
-
-/**
- * main - main function entry point of compiler
- * Return: value
-*/
 int main(void)
 {
 	FILE *file;
 
 	file = fopen("test.llc", "r");
 
-	lexer(file);
+	Token *tokens = lexer(file);
 }
