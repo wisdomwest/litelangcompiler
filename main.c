f7331af9eb3094675fe63d268202bd54fb1a2344
wisdomwest <harmonykoome@gmail.com>
2024-01-31 14:09:44 +0300

    still building up on the lexer now time to use token.h

--- main.c
+++ main.c
@@ -5,20 +5,51 @@
 #include "token.h"
 
 /**
+  * lexer - lexer function
+  * @file: pointer to llc file
+  * Return: void
+*/
+void lexer(FILE *file)
+{
+	char current;
+
+	current = fgetc(file);
+
+	while (current != EOF)
+	{
+		if (current == ';')
+		{
+			printf("FOUND SEMICOLON\n");
+		}
+		else if (current == '(')
+		{
+			printf("FOUND OPEN PAREN\n");
+		}
+		else if (current == ')')
+		{
+			printf("FOUND CLOSE PAREN\n");
+		}
+		else if (isdigit(current))
+		{
+			printf("FOUND DIGIT: %d\n", current - '0');
+		}
+		else if (isalpha(current))
+		{
+			printf("FOUND CHARACTER: %c\n", current);
+		}
+		current = fgetc(file);
+	}
+}
+
+/**
  * main - main function entry point of compiler
  * Return: value
 */
 int main(void)
 {
 	FILE *file;
-	char current;
 
 	file = fopen("test.llc", "r");
-	current = fgetc(file);
 
-	while(current != EOF)
-	{
-		printf("%c", current);
-		current = fgetc(file);
-	}
+	lexer(file);
 }
